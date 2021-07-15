cb959d5b9d68c0a78bcebf11c5b3ac0aab90a8ee
Thapanant Khongsattra <doki_mowmoe@outlook.com>
2021-07-15 22:05:31 +0700

    Add files via upload

---
+++ cat meowmeow.py
@@ -0,0 +1,8 @@
+#meowmeow
+word = str(input("what is your favorite pet? \n"))
+if word == "cat":
+    print("meowmeow!")
+if word == "dog":
+    print("wanwan!")
+if word == "miku":
+    print("GOD PET")
